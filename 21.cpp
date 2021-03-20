df09c372de756a768dc792aa9bc135a706cf74e5
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-03-20 21:18:19 +0530

    Write a program to print inverted half pyramid using numbers (different) (pattern question)

---
+++ 21.cpp
@@ -0,0 +1,18 @@
+// Write a program to print inverted half pyramid using numbers (different)(pattern question)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter Number :- ";
+    cin>>number;
+
+    for(int i=number;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
