a5276ce36c05e267e89a4eb128433fc0c5b9db02
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-03-20 21:19:31 +0530

    Write a program to print solid butterfly pattern (star pattern question)

---
+++ 24.cpp
@@ -0,0 +1,40 @@
+// Write a program to print solid butterfly pattern (star pattern question)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter Number :- ";
+    cin>>number;
+
+    for(int i=1;i<=number;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        int space=2*number-2*i;
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    for(int i=number;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        int space=2*number-2*i;
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
